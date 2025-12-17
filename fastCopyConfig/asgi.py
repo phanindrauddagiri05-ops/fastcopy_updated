5bbde08e2426ebe7260df6d6e47d9c540192f22d
phaniuddagiri <phanindrauddagiri05@gmail.com>
2025-12-17 20:32:12 +0530

    Ignore venv and prepare project structure

---
+++ fastCopyConfig/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for fastCopyConfig project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fastCopyConfig.settings')
+
+application = get_asgi_application()
